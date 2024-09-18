baa1832dcfb09edfd6a39d7505f2a249aa90265f
Adam Chrimes <iChrimes@live.co.uk>
2024-09-18 09:47:00 +0100

    add dummy login login

--- packages/login/src/index.js
+++ packages/login/src/index.js
@@ -3,7 +3,7 @@ import("./bootstrap");
 const appElement = document.querySelector("#dev-login");
 appElement.innerHTML = `
     <h1>Sign in</h1>
-    <form>
+    <form id="login-form">
       <div class="nhsuk-form-group">
         <label class="nhsuk-label" for="email">Email address</label>
         <input class="nhsuk-input" id="email" name="email" type="email" />
@@ -23,3 +23,32 @@ appElement.innerHTML = `
       </button>
     </form>
 `;
+
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  appElement.innerHTML = `
+    <h1>Hello, Adam Chrimes</h1>
+    <ul class="nhsuk-grid-row nhsuk-card-group">
+    <li class="nhsuk-grid-column-two-thirds nhsuk-card-group__item">
+
+      <div class="nhsuk-card nhsuk-card--clickable">
+        <div class="nhsuk-card__content nhsuk-card__content--primary">
+          <h2 class="nhsuk-card__heading nhsuk-heading-m nhsuk-u-margin-bottom-0">
+            <a class="nhsuk-card__link" href="#">Add new patient record</a>
+          </h2>
+          <svg class="nhsuk-icon" xmlns="http://www.w3.org/2000/svg" width="27" height="27" aria-hidden="true" focusable="false">
+            <circle cx="13.333" cy="13.333" r="13.333" fill="" />
+            <g data-name="Group 1" fill="none" stroke="#fff" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2.667">
+              <path d="M15.438 13l-3.771 3.771" />
+              <path data-name="Path" d="M11.667 9.229L15.438 13" />
+            </g>
+          </svg>
+
+        </div>
+      </div>
+
+    </li>
+  </ul>
+  <button class="nhsuk-button nhsuk-button--secondary" data-module="nhsuk-button" type="submit">Sign out</button>`;
+});
